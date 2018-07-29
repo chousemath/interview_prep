aa13b9233903a438f97cd4915a2091bbcf0f166b
Joseph Sungpil Choi <chousemath@gmail.com>
2018-07-29 11:32:11 +0900

    Quick save

--- ProblemMergingMeetingTimes/MergingMeetingTimes/Program.cs
+++ ProblemMergingMeetingTimes/MergingMeetingTimes/Program.cs
@@ -2,11 +2,34 @@
 
 namespace MergingMeetingTimes
 {
+    class Meeting
+    {
+        public int startTime { get; set; }
+        public int endTime { get; set; }
+        public Meeting(int _startTime, int _endTime)
+        {
+            startTime = _startTime;
+            endTime = _endTime;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Meeting[] meetings = {
+                new Meeting(0, 1),
+                new Meeting(3, 6),
+                new Meeting(3, 5),
+                new Meeting(4, 8),
+                new Meeting(10, 12),
+                new Meeting(9, 10)
+            };
+
+            for (int i = 0; i < meetings.Length; i++)
+            {
+                Console.WriteLine($"startTime: {meetings[i].startTime}, endTime: {meetings[i].endTime}");
+            }
         }
     }
 }
